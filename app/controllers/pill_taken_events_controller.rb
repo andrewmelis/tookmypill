dcc91c5e55b545830b70ae960b4b7774a2db77d2
andrew melis <andrewmelis@gmail.com>
2016-01-31 23:30:29 -0600

    remove security because it's annoying

--- app/controllers/pill_taken_events_controller.rb
+++ app/controllers/pill_taken_events_controller.rb
@@ -1,6 +1,4 @@
 class PillTakenEventsController < ApplicationController
-  http_basic_authenticate_with name: ENV["BASIC_AUTH_NAME"], password: ENV["BASIC_AUTH_PASSWORD"]
-
   # GET /pill_taken_events
   # GET /pill_taken_events.json
   def index
